93c9fc54f06074557686d50815e643048e505ce2
Dexaim <emilsmoney@live.com>
2022-11-26 04:43:36 -0500

    Fix test description

--- __tests__/db.test.js
+++ __tests__/db.test.js
@@ -1,7 +1,7 @@
 import { initQuery } from "../src/lib/planetscaleconn";
 
 describe("Test DB", () => {
-    it("should connect to db", async () => {
+    it("should connect to db and return first user named \"Dumb\"", async () => {
         const [rows] = await initQuery();
         expect(rows.first_name).toBe("Dumb");
     });
