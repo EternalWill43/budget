dddf9f72f453afebb73a95f79043a423a2b5c600
Dexaim <emilsmoney@live.com>
2022-11-26 05:47:47 -0500

    Add logging for creating access token

--- src/pages/api/exchange-public-token.js
+++ src/pages/api/exchange-public-token.js
@@ -1,13 +1,17 @@
 import { withIronSessionApiRoute } from 'iron-session/next';
 import { plaidClient, sessionOptions } from '../../lib/plaid';
+import { counter } from "../../lib/logstuff";
 
 export default withIronSessionApiRoute(exchangePublicToken, sessionOptions);
 
 async function exchangePublicToken(req, res) {
+  console.log("Hits on exchangePublicToken: ");
+  counter.increase('exchangePublicToken');
+  console.log(counter.exchangePublicToken);
   const exchangeResponse = await plaidClient.itemPublicTokenExchange({
     public_token: req.body.public_token,
   });
-
+  console.log(exchangeResponse.data.access_token + " is the access token");
   req.session.access_token = exchangeResponse.data.access_token;
   await req.session.save();
   res.send({ ok: true });
